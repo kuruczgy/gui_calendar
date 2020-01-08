555c03a7749d2d6a892a095da4052ec42d062c90
György Kurucz <kuruczgyurci@hotmail.com>
2020-01-08 23:10:35 +0100

    Fix missing break.

--- main.c
+++ main.c
@@ -109,6 +109,7 @@ int main(int argc, char **argv) {
 #ifdef CONFIG_BACKEND_FBDEV
         case BACKEND_FBDEV:
             backend = backend_init_fbdev();
+            break;
 #endif
         default:
             fprintf(stderr, "backend not supported\n");
